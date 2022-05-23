c13abe0d30ae0f8632db174d7e22228769b57e5e
Mikhail <mihail.zharavin65@gmail.com>
2022-05-23 13:39:07 +0300

    Added Router

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Cart from './components/pages/Cart';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
+import './scss/app.scss';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import Categories from '../Categories';
+import Sort from '../Sort';
+import Skeleton from '../PizzaBlock/Skeleton';
+import PizzaBlock from '../PizzaBlock/PizzaBlock';
+import axios from 'axios';
+
+const Home = () => {
+  const [itemsPizza, setItemsPizza] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get('https://628a8158e5e5a9ad322547c8.mockapi.io/items').then((response) => {
+      const { data } = response;
+      setItemsPizza(data);
+      setIsLoading(false);
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : itemsPizza.map((element) => {
+              return <PizzaBlock key={element.id} {...element} />;
+            })}
+      </div>
+    </>
+  );
+};
+
+export default Home;
